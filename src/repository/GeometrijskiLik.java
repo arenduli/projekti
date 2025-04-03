a309ac3bb71b2f8bad279b295d3b782343d93aed
anita <anita.rend@gmail.com>
2025-04-04 00:52:55 +0200

    task3 implementation

--- src/repository/GeometrijskiLik.java
+++ src/repository/GeometrijskiLik.java
@@ -1,7 +1,7 @@
 package repository;
 
 // bazna klasa koja sadrzi elemente koji su zajednicki u nekim drugim klasama
-public abstract class GeometrijskiLik {
+public abstract class GeometrijskiLik implements Comparable<GeometrijskiLik> {
 
     private String naziv;
     public abstract double povrsina();
@@ -15,5 +15,18 @@ public abstract class GeometrijskiLik {
         return "Naziv: " + this.naziv + "\n" +
                "Površina: " + povrsina() + "\n" +
                "Opseg: " + opseg() + "\n";
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    @Override
+    public int compareTo(GeometrijskiLik drugi) {    // compareTo metoda koja uspoređuje i sortira 2 geometrijska lika
+        return Double.valueOf(this.povrsina()).compareTo(Double.valueOf(drugi.povrsina()));
     }
 }
